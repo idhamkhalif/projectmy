29bc3d378b95241dacf8d6739f4383ae131ad5ad
idhamkhalif <midham.khalif@gmail.com>
2020-01-14 15:27:18 +0700

    add

---
+++ src/hello/Hello.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author idhamkhalif
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for(int x = 1; x < 11; x++)
+        {
+            System.out.println("Hello World "+x);
+        }
+    }
+    
+}
